188a6f81a6fb3daa0d172e51999e840a40c117fa
Ivanich41 <77790965+Ivanich41@users.noreply.github.com>
2021-09-04 23:27:39 +0300

    Добавил учёт дня недели
    Проверяет день недели, чтобы пары не образовались в субботу или воскресенье

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from loguru import logger as log
 import traceback
 
 import data
-
+import datetime
 
 log.add("debug.log", format="{time} {level} {message}", level="DEBUG", rotation="100 KB", compression="zip")
 
@@ -18,21 +18,30 @@ vk = vk_api.VkApi(token=data.TOKEN)
 vk_session = vk.get_api()
 # longpoll = VkBotLongPoll(vk, 206847683)    # <--- write id here
 
+def isworkday(): # эта функция проверят номер дня и не допускает работу бота  по субботам(5) и воскресеньям(6)
+    today = datetime.datetime.today().weekday()
+    if today != 5 or today !=  6 :
+        return True
+    else:
+        return False
+
 
 @log.catch()
 def run():
     while True:
         try:
-            # if '21:00:' not in str(datetime.now()):
-            r = vk.method('messages.send', {
-                'chat_id': 1,
-                'random_id': get_random_id(),
-                'message': None,
-                'attachment': 'video-206847683_456239017'
-            })
-            log.debug(r)
-            time.sleep(60*60*24)
-
+            if isworkday():
+                # if '21:00:' not in str(datetime.now()):
+                r = vk.method('messages.send', {
+                    'chat_id': 1,
+                    'random_id': get_random_id(),
+                    'message': None,
+                    'attachment': 'video-206847683_456239017'
+                })
+                log.debug(r)
+                time.sleep(60*60*24)
+            else:
+                time.sleep(60*60*24)
         except KeyboardInterrupt:
             log.info('Bot stopped')
             break
