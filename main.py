044b5938eb319a130faa6d6f97623f2e8f7c1312
Ivanich41 <77790965+Ivanich41@users.noreply.github.com>
2021-09-04 23:43:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ vk_session = vk.get_api()
 # longpoll = VkBotLongPoll(vk, 206847683)    # <--- write id here
 
 def isworkday(): # эта функция проверят номер дня и не допускает работу бота  по субботам(5) и воскресеньям(6)
-    return datetime.datetime.today().weekday() not in (5, 6)
+    return datetime.datetime.today().weekday() not in (5, 4)
 
 
 @log.catch()
